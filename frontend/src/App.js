02dd62cadbd886281254dd5a63063b4f1a6f4302
RanuRanjan <ranuranjan25@gmail.com>
2021-12-14 11:30:50 +0530

    deals day titles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { DayDeals } from './components/LandingPage/ProductsDeals/DealsOfDay';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+            <DayDeals/>
     </div>
   );
 }
---
+++ frontend/src/components/LandingPage/ProductsDeals/DealsOfDay.jsx
@@ -0,0 +1,9 @@
+import Styles from "./ProductDeals.module.css"
+
+export const  DayDeals=()=>{
+    return(
+        <div className={Styles.text_banner_title}>
+            <h4>Deals Of The Day</h4>
+        </div>
+    )
+}
