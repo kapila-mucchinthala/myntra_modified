e773e08e61c45431dc331309065072b72b14cc6f
RanuRanjan <ranuranjan25@gmail.com>
2021-12-15 10:35:33 +0530

    Biggest deals section completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { BigDeals } from './components/LandingPage/ProductsDeals/BiggestDeals';
 import { DayDeals } from './components/LandingPage/ProductsDeals/DealsOfDay';
 
 function App() {
   return (
     <div className="App">
             <DayDeals/>
+            <BigDeals/>
     </div>
   );
 }
