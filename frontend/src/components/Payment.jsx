57ba927cb9c8526fbfb01aa78667e24586433570
kapila-mucchinthala <kapilamucchinthala@gmail.com>
2021-12-16 16:37:32 +0530

    update a file

---
+++ frontend/src/components/Payment.jsx
@@ -0,0 +1,25 @@
+import './payment.css';
+
+export const Payment = () => {
+    return (
+        <div>
+            <div className="homeNavbar">
+               <div className="navbarDivOne">
+                 <img className="navbarDivOneLogo" src='https://www.linkpicture.com/q/logo-1_3.png' type="image" alt=''></img>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
