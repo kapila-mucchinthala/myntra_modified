565d46eff6735247c474d72c5eb3df38be639e8c
shivamfw10 <shivam_fw10_066@masai.school>
2021-12-16 16:11:50 +0530

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Foot } from './components/footer/Foot';
+import { Footer } from './components/footer/Footer';
 import { DayDeals } from './components/LandingPage/ProductsDeals/DealsOfDay';
 
 function App() {
   return (
-    <div className="App">
-            <DayDeals/>
+    <div>
+          {/* <DayDeals/> */}
+          {/* <Footer/> */}
+
+          <Foot/>
     </div>
   );
 }
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,58 @@
+import "./Footer.css";
+
+export const Footer = () => {
+    return (
+        <>
+            <footer class="footer">  
+                <ul class="r-footer">
+                    <li>
+                        <h2>ONLINE SHOPPING</h2>
+                        <ul class="box">
+                            <li><a href="#">Men</a></li>
+                            <li><a href="#">Women</a></li>
+                            <li><a href="#">Home & Living</a></li>
+                            <li><a href="#">Beauty</a></li>
+                            <li><a href="#">Gift Cards</a></li>
+                            <li><a href="#">Myntra Insider</a></li>
+                        </ul>
+                    </li>
+                    <li class="features">
+                        <h2>USEFUL LINKS</h2>
+                        <ul class="box h-box">
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">T&C</a></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Track Orders</a></li>
+                            <li><a href="#">Shipping</a></li>
+                            <li><a href="#">Cancellation</a></li>
+                            <li><a href="#">Returns</a></li>
+                            <li><a href="#">Whitehat</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h2>Legal</h2>
+                        <ul class="box">
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Contract</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="l-footer">
+                    <h1>
+                        <img src="https://i.postimg.cc/y62wcLBq/logo.png" alt=""/></h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam atque recusandae in sit sunt molestiae aliquid fugit. Mollitia eaque tempore iure sit nobis? Vitae nemo, optio maiores numquam quis recusandae.</p>
+                </div>
+                <div class="b-footer">
+                    <p>
+                        All rights reserved by ©CompanyName 2020 </p>
+                </div>
+            </footer>
+
+        </>
+    )
+}
