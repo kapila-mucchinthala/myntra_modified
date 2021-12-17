52983aa576bb4dbfb10727f13b6249339bd660e2
RaniKamna <kamna.a139@gmail.com>
2021-12-18 01:21:38 +0530

    routing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
-import {LandingPage} from "./components/Body/LandingPage"
-import {Login} from "./components/Login/login"
-import { VerifyOtp } from "./components/Login/VerifyOtp"
+import { LandingPage } from "./components/Body/LandingPage";
+import { Login } from "./components/Login/login";
+import { VerifyOtp } from "./components/Login/VerifyOtp";
+import { Route, Switch } from 'react-router-dom';
+import { Productscreen } from "./components/ProductScreen/Productscreen";
+
 function App() {
   return (
     <>
-  <LandingPage/>
-  {/* <Login/> */}
-  {/* <VerifyOtp/> */}
+      {/* <LandingPage /> */}
+      {/* <Login/> */}
+      {/* <VerifyOtp/> */}
+
+      <Switch>
+        <Route path='/' exact>
+          <LandingPage />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/otp'>
+          <VerifyOtp />
+        </Route>
+        <Route path='/sweatshirt'>
+          <Productscreen />
+        </Route>
+      </Switch>
     </>
   );
 }
