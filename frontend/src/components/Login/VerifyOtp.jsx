fed1f13cd82b7cf97e52a8c008fdbea0dd1c229f
RanuRanjan <ranuranjan25@gmail.com>
2021-12-17 23:09:35 +0530

    login and verify otp static

--- frontend/src/components/Login/VerifyOtp.jsx
+++ frontend/src/components/Login/VerifyOtp.jsx
@@ -2,41 +2,62 @@ import Styles from "./login.module.css";
 
 export const VerifyOtp = () => {
   return (
-    <div >
+    <div>
       <div className={Styles.loginPage}>
         <div className={Styles.login_card}>
-          <div className={Styles.login_card_image}>
-            <img
-              src="https://constant.myntassets.com/pwa/assets/img/3a438cb4-c9bf-4316-b60c-c63e40a1a96d1548071106233-mobile-verification.jpg"
-              alt="otpImg"
-              width="88"
-              height="88"
-            />
-          </div>
-          <div className={Styles.login_form}>
-            <div className={Styles.Loginform_title}>
-              <p>
-                Verify with OTP 
-                </p>
-              <br />
-              <span>Sent to {}</span>
-              
+          <div style={{padding:"10px"}} className={Styles.Verifiction_form}>
+            <div >
+              <img
+                src="https://constant.myntassets.com/pwa/assets/img/3a438cb4-c9bf-4316-b60c-c63e40a1a96d1548071106233-mobile-verification.jpg"
+                alt="otpImg"
+                width="90"
+                height="90"
+              />
+            </div>
+            <div
+              style={{ marginTop: "20px" }}
+              className={Styles.Loginform_title}
+            >
+              <p>Verify with OTP</p>
+             
+              <h4 style={{fontSize:"14px",fontWeigth:"400",opacity:"0.7" }} >Sent to 8770719228</h4>
             </div>
             <div className={Styles.formInput}>
               <div>
-                <div className={Styles.OtpContainer}> 
-                <input type="tel"  maxLength="1" data-val="0" autocomplete="off"/>
-                <input type="tel"  maxLength="1" data-val="0" autocomplete="off"/>
-                <input type="tel"  maxLength="1" data-val="0" autocomplete="off"/>
-                <input type="tel"  maxLength="1" data-val="0" autocomplete="off"/>
-                  
-                  
+                <div className={Styles.OtpContainer}>
+                  <input
+                    type="tel"
+                    maxLength="1"
+                    data-val="0"
+                    autocomplete="off"
+                  />
+                  <input
+                    type="tel"
+                    maxLength="1"
+                    data-val="0"
+                    autocomplete="off"
+                  />
+                  <input
+                    type="tel"
+                    maxLength="1"
+                    data-val="0"
+                    autocomplete="off"
+                  />
+                  <input
+                    type="tel"
+                    maxLength="1"
+                    data-val="0"
+                    autocomplete="off"
+                  />
                 </div>
               </div>
             </div>
-            
-            <div className={Styles.CotinueBtnDiv}>
-            RESEND OTP
+
+            <div className={Styles.Gethelp}>
+              <span>RESEND OTP</span>
+            </div>
+            <div className={Styles.Gethelp}>
+              Log in using<span> Password</span>
             </div>
             <div className={Styles.Gethelp}>
               Have trouble logging in ?<span> Get help</span>
