94253dd517a5c8200f5db1a9f5190354c18d87f6
RanuRanjan <ranuranjan25@gmail.com>
2021-12-18 15:56:56 +0530

    add address and css

--- frontend/src/components/Address/Address.jsx
+++ frontend/src/components/Address/Address.jsx
@@ -1,18 +1,20 @@
-import './address.css';
- 
-export const Address = () => {
-    return (
+import '../Payment/payment.css'
+import Styles from './Address.module.css'
+import './Selectaddress.css';
+
+export const Address = ()=>{
+    return(
         <div>
-            <div className="homeNavbar">
+            <div  className="homeNavbar">
                <div className="navbarDivOne">
                  <img className="navbarDivOneLogo" src='https://www.linkpicture.com/q/logo-1_3.png' type="image" alt=''></img>
-                </div> 
+                </div>
                 <div className='paymentnavbarSection'>
                     <div id='paymentnavbarMid1'>BAG</div>
                     <div className='paymentnavbarSecDots'>----------</div>
                     <div id='paymentnavbarMid2' className='paymentNavbarcolor'>ADDRESS</div>
                     <div className='paymentnavbarSecDots'>----------</div>
-                    <div id='paymentnavbarMid3'>PAYMENT</div>
+                    <div id='paymentnavbarMid3' >PAYMENT</div>
                 </div>
                 <div className='paymentnavbarSection' id='paymentnavbarSectionlastDiv'>
                     <div>
@@ -20,46 +22,54 @@ export const Address = () => {
                     </div>
                     <div>100% SECURE</div>
                 </div>
-                
-            </div>
-            <hr className="horizontalLine1"></hr>     
+            </div> 
+            
+           <div className={Styles.SaleEnds}>
+               <p>Sale Ends <span>00</span> Hrs: <span>33 </span>Min : <span>54</span>Sec</p>
+           </div>
 
-            <div className='paymentPage'>
-                <div>
-                    <div className='delivery'>
-                        <h5 className='selectH'>Select Delivery Address</h5>
-                        <button className='newAddressBtn'>ADD NEW ADDRESS</button>
-                    </div>
-                    <h5 className='heading'>DEFAULT ADDRESS</h5>
-                    <div className='bankOffers'>
+
+            <div className={Styles.AddressContainer}>
+                <div className={Styles.ContactdetailsForm} >
                         <div>
-                          <div className='bankOffersLine'>
-                            <div className='bankOffersLineImg'>
-                                <img src='https://www.linkpicture.com/q/1_280.png' alt='' />
-                            </div>
-                            <div>Rahul Raj</div>
-                            </div>
-                            <div className='bankOffersP'>C-48, Noida Sector 91</div>
-                            <div className='bankOffersP'>Noida, Uttar Pradesh</div>
-                            <p className='bankOffersP'>Mobile: 8799094251</p>
-                            <ul className='bankOffersUl'><li>Pay on Delivery available</li></ul>
-                            <div className='deliveryBtns'>
-                                <button className='delivBtn'>REMOVE</button>
-                                <button className='delivBtn'>EDIT</button>
-                            </div>
-                            
-                        </div>    
-                    </div>
-                    
-                    <div className='giftDiv'>                     
-                        <div className='giftCardBtn'>+ Add New Address</div>
-                    </div>
-                </div>
+                           <label  htmlFor="" > Contact details</label>
+                        </div>
+                        <form>
+                            <input type="text" placeholder='Name*' required/>
+                            <br /><br />
+                            <input type="text" placeholder='Mobile No* ' />
+                            <br /><br />
+                            <label htmlFor="">Address</label><br />
+                            <input type="text"  placeholder='Pin code*' required/><br /> <br />
+                            <input type="text" placeholder='Address (House No. Building Street,Area)*' required /><br /> <br />
+                            <input type="text" placeholder='Locality / Town*'/><br /><br />
+                            <div className={Styles.CityState}>
+                            <input type="text" placeholder='City / District*' required/>
+                            <input type="text" placeholder='State*' required />
+                            </div><br />
+                           
+                            <label htmlFor="">Save Address</label>
 
-                <div className="verticalLine1"></div>
-                <div>
+                           <div className={Styles.HomeWorkbtn}>
+                            <button>Home</button>
+                            <button>Work</button><br />
+                           </div>
+                        
+                            <div className={Styles.Checkbox}>
+                            <input type="checkbox"/>
+                            <label htmlFor=""> Make this my default address</label>
+                            </div><br />
+                            
+                           
+
+                            <button className={Styles.Addressbtn} type='submit'>Add Address</button>
+                        </form>
+                </div>
+                <div className={Styles.PriceDetails}>
+                  <div>
+                  <div> 
                     <div className='priceDetails'>
-                        <div className='deliveeyEstimate'>DELIVERY ESTIMATES</div>
+                        <div className='deliveeyEstimate'>PRICE DEATILS (1 item)</div>
                         <div className='estimatedDeliv'>
                             <img className='estimatedDelivImg' src='https://www.linkpicture.com/q/2_229.png'></img>
                             <div className='estimLine'>Estimated delivery by <span className='estmatedSpan'>3Dec 2021</span></div>
@@ -83,28 +93,19 @@ export const Address = () => {
                                 <div id='totalPricecolor'>Total Amount</div>
                                 <div id='totalPricecolor'>Rs. 780</div>
                             </div>
-                            <button className='continueBtn'>CONTINUE</button>
+                            
                         </div>
                     </div>
                 </div>
-            </div>
-            {/* <hr className='hr4'></hr> */}
-            <div className='cardsDiv'>
-                <div className='cardFlex'>
-                    <img className='cardImg' src='https://www.linkpicture.com/q/1_288.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/2_230.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/3_114.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/4_78.png' alt=''/>
-                    <img className='cardImg' src='https://www.linkpicture.com/q/5_72.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/6_65.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/7_39.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/8_36.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/9_35.png' alt='' />
-                    <img className='cardImg' src='https://www.linkpicture.com/q/10_41.png' alt=''/>
+                  </div>
                 </div>
-                <div className='needHelp'>Need Help? Contact Us.</div>
             </div>
-        </div>
-        
+
+
+
+
+
+
+        </div> 
     )
 }
