b3c2c21034c9342a9b066eda10c6ef21bab2e023
kapila-mucchinthala <kapilamucchinthala@gmail.com>
2021-12-18 13:27:19 +0530

    Address Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
-
-// import { LandingPage } from './components/Body/LandingPage';
-// import { ProductView } from './components/ProductViewPage/ProductView';
-import { Address } from './components/Address/Address';
-
-function App() {
-  return (
-    <>
-    {/* <LandingPage/> */}
-      {/* <ProductView/> */}
-      <Address/>
-=======
 import { LandingPage } from "./components/Body/LandingPage";
 import { Login } from "./components/Login/login";
 import { VerifyOtp } from "./components/Login/VerifyOtp";
 import { Route, Switch } from 'react-router-dom';
 import { Productscreen } from "./components/ProductScreen/Productscreen";
-import { Address } from "./components/Address/Address";
 
 function App() {
   return (
     <>
-      {/* <LandingPage /> */}
-      {/* <Login/> */}
-      {/* <VerifyOtp/> */}
-     
-
       <Switch>
         <Route path='/' exact>
           <LandingPage />
@@ -39,11 +20,7 @@ function App() {
         <Route path='/sweatshirt'>
           <Productscreen />
         </Route>
-        <Route path='/address'>
-           <Address/> 
-        </Route>
       </Switch>
->>>>>>> 4fab9160685bef4b47a6f074165eed1624a97135
     </>
   );
 }
--- frontend/src/components/Address/Address.jsx
+++ frontend/src/components/Address/Address.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './address.css';
  
 export const Address = () => {
@@ -8,27 +7,12 @@ export const Address = () => {
                <div className="navbarDivOne">
                  <img className="navbarDivOneLogo" src='https://www.linkpicture.com/q/logo-1_3.png' type="image" alt=''></img>
                 </div> 
-=======
-import '../Payment/payment.css'
-import Styles from './Address.module.css'
-export const Address = ()=>{
-    return(
-        <div>
-            <div  className="homeNavbar">
-               <div className="navbarDivOne">
-                 <img className="navbarDivOneLogo" src='https://www.linkpicture.com/q/logo-1_3.png' type="image" alt=''></img>
-                </div>
->>>>>>> 4fab9160685bef4b47a6f074165eed1624a97135
                 <div className='paymentnavbarSection'>
                     <div id='paymentnavbarMid1'>BAG</div>
                     <div className='paymentnavbarSecDots'>----------</div>
                     <div id='paymentnavbarMid2' className='paymentNavbarcolor'>ADDRESS</div>
                     <div className='paymentnavbarSecDots'>----------</div>
-<<<<<<< HEAD
                     <div id='paymentnavbarMid3'>PAYMENT</div>
-=======
-                    <div id='paymentnavbarMid3' >PAYMENT</div>
->>>>>>> 4fab9160685bef4b47a6f074165eed1624a97135
                 </div>
                 <div className='paymentnavbarSection' id='paymentnavbarSectionlastDiv'>
                     <div>
@@ -36,7 +20,6 @@ export const Address = ()=>{
                     </div>
                     <div>100% SECURE</div>
                 </div>
-<<<<<<< HEAD
                 
             </div>
             <hr className="horizontalLine1"></hr>     
@@ -123,61 +106,5 @@ export const Address = ()=>{
             </div>
         </div>
         
-=======
-            </div> 
-            
-           <div className={Styles.SaleEnds}>
-               <p>Sale Ends <span>00</span> Hrs: <span>33 </span>Min : <span>54</span>Sec</p>
-           </div>
-
-
-            <div className={Styles.AddressContainer}>
-                <div className={Styles.ContactdetailsForm} >
-                        <div>
-                           <label  htmlFor="" > Contact details</label>
-                        </div>
-                        <form>
-                            <input type="text" placeholder='Name*' required/>
-                            <br /><br />
-                            <input type="text" placeholder='Mobile No* ' />
-                            <br /><br />
-                            <label htmlFor="">Address</label><br />
-                            <input type="text"  placeholder='Pin code*' required/><br /> <br />
-                            <input type="text" placeholder='Address (House No. Building Street,Area)*' required /><br /> <br />
-                            <input type="text" placeholder='Locality / Town*'/><br /><br />
-                            <div className={Styles.CityState}>
-                            <input type="text" placeholder='City / District*' required/>
-                            <input type="text" placeholder='State*' required />
-                            </div><br />
-                           
-                            <label htmlFor="">Save Address</label>
-
-                           <div className={Styles.HomeWorkbtn}>
-                            <button>Home</button>
-                            <button>Work</button><br />
-                           </div>
-                        
-                            <div className={Styles.Checkbox}>
-                            <input type="checkbox"/>
-                            <label htmlFor=""> Make this my default address</label>
-                            </div><br />
-                            
-                           
-
-                            <button className={Styles.Addressbtn} type='submit'>Add Address</button>
-                        </form>
-                </div>
-                <div className={Styles.PriceDetails}>
-                   
-                </div>
-            </div>
-
-
-
-
-
-
-        </div> 
->>>>>>> 4fab9160685bef4b47a6f074165eed1624a97135
     )
 }
